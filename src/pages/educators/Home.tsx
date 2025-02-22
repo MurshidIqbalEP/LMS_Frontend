6d551eebfd8a306bb17348bf2e55d83dbeab7b90
Murshid <murshidm2x@gmail.com>
2025-02-22 16:17:44 +0530

    fixed bug while unmounding home page

--- src/pages/educators/Home.tsx
+++ src/pages/educators/Home.tsx
@@ -1,6 +1,4 @@
 import Hero from "../../componets/educators/Hero"
-import MagikScroll from "../../componets/students/MagikScroll"
-import Topcourses from "../../componets/students/Topcourses"
 import HorizondalScroll from "../../componets/educators/HorizondalScroll"
 
 function Home() {
