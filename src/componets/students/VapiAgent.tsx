c04fe997a935c7ed6566d66d9b6d2f0f412e2e2c
Murshid <murshidm2x@gmail.com>
2025-04-14 20:50:18 +0530

    vapi integrated

--- src/componets/students/VapiAgent.tsx
+++ src/componets/students/VapiAgent.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { startAssistant } from "../../services/voiceAgent";
+import  Vapi  from '@vapi-ai/web'
+export const vapi = new Vapi(import.meta.env.VITE_VAPI_KEY);
 
 interface Props {
   question: string;
@@ -9,9 +11,53 @@ interface Props {
 const VapiAgent: React.FC<Props> = ({ question, onUserAnswer }) => {
 
   useEffect(() => {
-    console.log(startAssistant);
+    vapi.start({
+      transcriber: {
+        provider: "deepgram",
+        model: "nova-2",
+        language: "en-US",
+      },
+      model: {
+        provider: "openai",
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content: `
+You are an AI voice assistant conducting interviews.
+Your job is to ask candidates provided interview questions, assess their responses.
+Begin the conversation with a friendly introduction, setting a relaxed yet professional tone. Example:
+"Hey there! Welcome to your  interview. Let’s get started with a few questions!"
+Ask one question at a time and wait for the candidate’s response before proceeding. Keep the questions clear and concise. Below Are the questions ask one by one:
+Questions: `+question+`
+If the candidate struggles, offer hints or rephrase the question without giving away the answer. Example:
+"Need a hint? Think about how React tracks component updates!"
+Provide brief, encouraging feedback after each answer. Example:
+"Nice! That’s a solid answer."
+"Hmm, not quite! Want to try again?"
+Keep the conversation natural and engaging—use casual phrases like "Alright, next up..." or "Let’s tackle a tricky one!"
+After 5-7 questions, wrap up the interview smoothly by summarizing their performance. Example:
+"That was great! You handled some tough questions well. Keep sharpening your skills!"
+End on a positive note:
+"Thanks for chatting! Hope to see you crushing projects soon!"
+Key Guidelines:
+✅ Be friendly, engaging, and witty ✍️
+✅ Keep responses short and natural, like a real conversation
+✅ Adapt based on the candidate’s confidence level
+✅ Ensure the interview remains focused on React
+        `.trim(),
+          },
+        ],
+      },
+      voice: {
+        provider: "playht",
+        voiceId: "jennifer",
+      },
+      name: "My Inline Assistant",
     
-    startAssistant()
+    });
+    
+
   }, [question]);
 
   return (
